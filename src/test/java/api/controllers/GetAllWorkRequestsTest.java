bae6365ea722d80439b8b0147a4f89fc4e71045c
NAROMBA CONDE <amba@MacBook-Air-de-NAROMBA.local>
2024-11-22 16:17:20 -0500

    Modification des tests unitaires

--- src/test/java/api/controllers/GetAllWorkRequestsTest.java
+++ src/test/java/api/controllers/GetAllWorkRequestsTest.java
@@ -10,6 +10,12 @@ import api.DatabaseManager;
 
 import java.time.LocalDate;
 import java.util.List;
+import static org.mockito.Mockito.*; 
+import org.mockito.Mock;             
+import org.mockito.Mockito;          
+import org.junit.jupiter.api.Test;   
+import org.junit.jupiter.api.extension.ExtendWith; 
+import org.mockito.junit.jupiter.MockitoExtension; 
 
 import static org.junit.jupiter.api.Assertions.*;
 
